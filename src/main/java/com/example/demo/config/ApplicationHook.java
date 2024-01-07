095d5f9757658c9b5e60e02ee4ff8a22a9f16179
Noman Ali Abbasi <noman.ali.abbasy@gmail.com>
2024-01-07 14:40:54 +0300

    Excluded spring default logging and added context closed event to unload the context

--- src/main/java/com/example/demo/config/ApplicationHook.java
+++ src/main/java/com/example/demo/config/ApplicationHook.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
-import com.example.demo.util.LogUtils;
+import com.example.demo.utils.LogUtils;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
