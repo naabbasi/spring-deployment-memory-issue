6c05459621bf46a9056ec6eaaf92c6556d479940
Noman Ali <noman.ali.abbasy@gmail.com>
2024-02-05 21:05:21 +0300

    Using same Database configuration

--- src/main/java/com/example/demo/config/ApplicationHook.java
+++ src/main/java/com/example/demo/config/ApplicationHook.java
@@ -6,25 +6,23 @@ import java.util.Objects;
 
 import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LoggerContext;
-import java.util.Objects;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
-
-import com.example.demo.utils.LogUtils;
 
 @Configuration
 public class ApplicationHook implements ApplicationListener<ContextClosedEvent> {
     private final WebApplicationContext webApplicationContext;
+    private final GenericApplicationContext genericApplicationContext;
     private final LogUtils logUtils;
 
-    public ApplicationHook(WebApplicationContext webApplicationContext, LogUtils logUtils) {
+    public ApplicationHook(WebApplicationContext webApplicationContext, GenericApplicationContext genericApplicationContext, LogUtils logUtils) {
         this.webApplicationContext = webApplicationContext;
+        this.genericApplicationContext = genericApplicationContext;
         this.logUtils = logUtils;
     }
 
@@ -32,8 +30,11 @@ public class ApplicationHook implements ApplicationListener<ContextClosedEvent> 
     public void onApplicationEvent(ContextClosedEvent event) {
         this.logUtils.log("ContextClosedEvent called");
         try {
-            ContextLoader contextLoader = new ContextLoader();
-            contextLoader.closeWebApplicationContext(Objects.requireNonNull(this.webApplicationContext.getServletContext()));
+            /*ContextLoader contextLoader = new ContextLoader();
+            contextLoader.closeWebApplicationContext(Objects.requireNonNull(this.webApplicationContext.getServletContext()));*/
+
+            //Both are working fine
+            this.genericApplicationContext.close();
 
             LoggerContext loggerContext = LoggerContext.getContext();
             org.apache.logging.log4j.core.config.Configuration configuration = loggerContext.getConfiguration();
