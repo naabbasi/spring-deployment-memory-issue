f0b5b12841ffefcbd59fec8e1398348b45caf5d0
Noman Ali Abbasi <noman.ali.abbasy@gmail.com>
2024-02-13 10:40:20 +0300

    Update ApplicationContext.java

--- src/main/java/com/example/demo/config/ApplicationContext.java
+++ src/main/java/com/example/demo/config/ApplicationContext.java
@@ -19,8 +19,53 @@ public class ApplicationContext implements ServletContextListener {
     public void contextDestroyed(ServletContextEvent sce) {
         logUtils.log("ApplicationContext contextDestroyed called");
         ServletContextListener.super.contextDestroyed(sce);
-        /*WebApplicationContext webApplicationContext = WebApplicationContextUtils.getWebApplicationContext(sce.getServletContext());
-        FastThreadLocal.removeAll();
-        FastThreadLocal.destroy();*/
+        this.cleanThreadLocals();
+    }
+
+    private void cleanThreadLocals() {
+        try {
+            // Get a reference to the thread locals table of the current thread
+            Thread thread = Thread.currentThread();
+            Field threadLocalsField = Thread.class.getDeclaredField("threadLocals");
+            threadLocalsField.setAccessible(true);
+            Object threadLocalTable = threadLocalsField.get(thread);
+
+            System.out.println("============================= \nThread Locals -> " + threadLocalTable + "\n=============================");
+
+            // Get a reference to the array holding the thread local variables inside the
+            // ThreadLocalMap of the current thread
+            Class threadLocalMapClass = Class.forName("java.lang.ThreadLocal$ThreadLocalMap");
+            Field tableField = threadLocalMapClass.getDeclaredField("table");
+            tableField.setAccessible(true);
+            Object table = tableField.get(threadLocalTable);
+
+            System.out.println("============================= \n ThreadLocalMapClass -> " + threadLocalMapClass + "\n=============================");
+
+            // The key to the ThreadLocalMap is a WeakReference object. The referent field of this object
+            // is a reference to the actual ThreadLocal variable
+            Field referentField = Reference.class.getDeclaredField("referent");
+            referentField.setAccessible(true);
+
+            for (int i = 0; i < Array.getLength(table); i++) {
+                // Each entry in the table array of ThreadLocalMap is an Entry object
+                // representing the thread local reference and its value
+                Object entry = Array.get(table, i);
+                if (entry != null) {
+                    // Get a reference to the thread local object and remove it from the table
+                    ThreadLocal threadLocal = (ThreadLocal)referentField.get(entry);
+                    if(threadLocal.get().getClass().getClassLoader() == Thread.currentThread().getContextClassLoader()){
+                        System.out.println("Web Application ThreadLocal");
+                    } else {
+                        System.out.println("Tomcat ThreadLocal");
+                    }
+                    System.out.println("============================= \n ThreadLocal -> " + threadLocal + "\n=============================");
+                    threadLocal.remove();
+
+                }
+            }
+        } catch(Exception e) {
+            // We will tolerate an exception here and just log it
+            throw new IllegalStateException(e);
+        }
     }
 }
