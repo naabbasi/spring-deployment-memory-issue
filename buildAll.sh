03c0a8ac31d3515fa85cfab0ac6dde3f0d916b2d
Noman Ali <noman.ali.abbasy@gmail.com>
2024-08-04 23:25:21 +0300

    Added projects to work on memory leak fix

--- buildAll.sh
+++ buildAll.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 export current_dir=$(pwd)
 
-cd vanila-spring-boot-2-7-16
+cd vanila-spring-boot-2.7.16
 mvn clean package
 cd target && rm -f vanila-spring-boot-2.7.16-0.0.5.war
 mv vanila-spring-boot-2.7.16-0.0.5.war.original vanila-spring-boot-2.7.16-0.0.5.war
@@ -12,3 +12,10 @@ cd spring-boot-2-7-16-actuator-micrometer
 mvn clean package
 cd target && rm -f spring-boot-2-7-16-actuator-micrometer-0.0.5.war
 mv spring-boot-2-7-16-actuator-micrometer-0.0.5.war.original spring-boot-2-7-16-actuator-micrometer-0.0.5.war
+
+cd $current_dir
+
+cd spring-boot-2-7-16-log4j-sync
+mvn clean package
+cd target && rm -f spring-boot-2-7-16-log4j-sync-0.0.5.war
+mv spring-boot-2-7-16-log4j-sync-0.0.5.war.original spring-boot-2-7-16-log4j-sync-0.0.5.war
---
+++ spring-boot-2-7-16-log4j-sync/src/main/java/com/example/demo/utils/LogUtils.java
@@ -0,0 +1,28 @@
+package com.example.demo.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogUtils {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void log(String message, Object... params) {
+        if(params != null && params.length > 0) {
+            logger.info(message, params);
+            return;
+        }
+
+        logger.info(message);
+    }
+
+    /*public void log(String message, Object... params) {
+        if(params != null && params.length > 0) {
+            System.out.printf("%s %s\n", message, Arrays.toString(params));
+            return;
+        }
+
+        System.out.printf("%s \n", message);
+    }*/
+}
